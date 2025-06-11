e09cfb0adb579142e0934cf39b743d59c9d2cb3c
Jitendra sah <jitendra.sah@sachtechsolution.com>
2025-06-11 16:00:30 +0530

    added firebase service account to dotenv

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,5 +1,8 @@
 import admin from 'firebase-admin';
-import serviceAccount from '../sensitiveData/firebaseAdmin.json' assert { type: 'json' };
+import dotenv from 'dotenv';
+dotenv.config();
+// import serviceAccount from '../sensitiveData/firebaseAdmin.json' assert { type: 'json' };
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
